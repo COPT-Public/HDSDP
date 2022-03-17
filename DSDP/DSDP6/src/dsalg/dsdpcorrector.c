3fdac25b9978ffe9830227552897079a3a304008
Wpxzgwz <gwz@163.shufe.edu.cn>
2022-03-17 09:50:06 +0800

    Update parameter notes

---
+++ DSDP/DSDP6/src/dsalg/dsdpcorrector.c
@@ -0,0 +1,8 @@
+//
+//  dsdpCorrector.c
+//  DSDP6
+//
+//  Created by 高文智 on 2022/3/16.
+//
+
+#include "dsdpCorrector.h"
---
+++ DSDP/DSDP6/src/dsalg/dsdpcorrector.h
@@ -0,0 +1,13 @@
+//
+//  dsdpCorrector.h
+//  DSDP6
+//
+//  Created by 高文智 on 2022/3/16.
+//
+
+#ifndef dsdpCorrector_h
+#define dsdpCorrector_h
+
+#include <stdio.h>
+
+#endif /* dsdpCorrector_h */
