ec9fad0d6779da2d93be3cc776467ec8ee61041e
Wpxzgwz <gwz@163.shufe.edu.cn>
2022-11-26 21:16:42 +0800

    Update header for vector operations

--- hdsdp/hdsdp/linalg/vec_opts.h
+++ hdsdp/hdsdp/linalg/vec_opts.h
@@ -2,5 +2,14 @@
 #define vec_opts_h
 
 
+extern double nrm2( int *n, double *x, int *incx );
+extern void axpy( int *n, double *a, double *x, int *incx, double *y, int *incy );
+extern double dot( int *n, double *x, int *incx, double *y, int *incy );
+extern void scal( int *n, double *sa, double *sx, int *incx );
+extern void rscl( int *n, double *sa, double *sx, int *incx );
+extern int idamax( int *n, double *x, int *incx );
+extern double sumlogdet( int *n, double *x );
+extern void vvscl( int *n, double *s, double *x );
+extern void vvrscl( int *n, double *s, double *x );
 
 #endif /* vec_opts_h */
