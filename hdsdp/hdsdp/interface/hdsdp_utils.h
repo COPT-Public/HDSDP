cb442e9187b0a9bcac651630cb5db4b2b003e599
Wpxzgwz <gwz@163.shufe.edu.cn>
2022-11-26 16:00:12 +0800

    Replace def_hdsdp by hdsdp

--- hdsdp/hdsdp/interface/hdsdp_utils.h
+++ hdsdp/hdsdp/interface/hdsdp_utils.h
@@ -1,8 +1,14 @@
+/** @file hdsdp\_utils.h
+ *  @brief Define utilities for HDSDP
+ *  @date 11/24/2022
+ */
+
 #ifndef hdsdp_utils_h
 #define hdsdp_utils_h
 
-#include "def_hdsdp.h"
+#include "hdsdp.h"
 
+/* Define macros */
 #define HDSDP_FREE(var) do {free((var)); (var) = NULL;} while (0)
 #define HDSDP_INIT(var, type, size) (var) = (type *) calloc(size, sizeof(type))
 #define HDSDP_MEMCPY(dst, src, type, size) memcpy(dst, src, sizeof(type) * (size))
