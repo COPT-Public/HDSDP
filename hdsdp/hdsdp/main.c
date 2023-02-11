c60560c64167e75810c16ffa4d1f7ef446b1df4a
Wpxzgwz <gwz@163.shufe.edu.cn>
2023-02-11 22:32:30 +0800

    Update main script

--- hdsdp/hdsdp/main.c
+++ hdsdp/hdsdp/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int test_file_io( void );
+int test_file_io( char *fname );
 
 int main(int argc, const char * argv[]) {
-    
-    test_file_io();
+ 
+    char *fname = "/Users/gaowenzhi/Desktop/dsdp6/hsd/benchmark/sdplib/hinf11.dat-s";
+    return test_file_io(fname);
 }
