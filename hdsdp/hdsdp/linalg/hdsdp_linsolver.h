a9542a9657cb92e74a12fe4975b5d2f0ec8911f1
Wenzhi Gao <gwz@163.shufe.edu.cn>
2023-02-12 21:24:10 +0800

    Update linear system header

--- hdsdp/hdsdp/linalg/hdsdp_linsolver.h
+++ hdsdp/hdsdp/linalg/hdsdp_linsolver.h
@@ -1,13 +1,26 @@
 #ifndef hdsdp_linsolver_h
 #define hdsdp_linsolver_h
 
+#include "linalg/def_hdsdp_linsolver.h"
+
 typedef hdsdp_linsys_fp hdsdp_linsys;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+extern hdsdp_retcode HFpLinsysCreate( hdsdp_linsys_fp **pHLin, int nCol, linsys_type Ltype );
+extern void HFpLinsysSetParam( hdsdp_linsys_fp *HLin, double relTol, double absTol, int nThreads, int maxIter, int nRestartFreq );
+extern hdsdp_retcode HFpLinsysSymbolic( hdsdp_linsys_fp *HLin, int *colMatBeg, int *colMatIdx );
+extern hdsdp_retcode HFpLinsysNumeric( hdsdp_linsys_fp *HLin, int *colMatBeg, int *colMatIdx, double *colMatElem );
+extern hdsdp_retcode HFpLinsysPsdCheck( hdsdp_linsys_fp *HLin, int *colMatBeg, int *colMatIdx, double *colMatElem, int *isPsd );
+extern void HFpLinsysFSolve( hdsdp_linsys_fp *HLin, int nRhs, double *rhsVec, double *solVec );
+extern void HFpLinsysBSolve( hdsdp_linsys_fp *HLin, int nRhs, double *rhsVec, double *solVec );
+extern hdsdp_retcode HFpLinsysSolve( hdsdp_linsys_fp *HLin, int nRhs, double *rhsVec, double *solVec );
+extern hdsdp_retcode HFpLinsysGetDiag( hdsdp_linsys_fp *HLin, double *diagElem );
+extern void HFpLinsysInvert( hdsdp_linsys_fp *HLin, double *dFullMatrix, double *dAuxiMatrix );
+extern void HFpLinsysClear( hdsdp_linsys_fp *HLin );
+extern void HFpLinsysDestroy( hdsdp_linsys_fp **HLin );
 
 #ifdef __cplusplus
 }
