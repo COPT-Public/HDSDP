5e0faa3edf590fabe1f1012f7868cb8d6ab47c4a
Wenzhi Gao <gwz@163.shufe.edu.cn>
2023-08-08 17:38:47 +0800

    Use reduced accuracy

--- hdsdp/hdsdp/interface/hdsdp.c
+++ hdsdp/hdsdp/interface/hdsdp.c
@@ -342,10 +342,10 @@ static void HDSDPIGetDefaultParams( hdsdp *HSolver ) {
     set_int_param(HSolver, INT_PARAM_CORRECTORB, 12);
     set_int_param(HSolver, INT_PARAM_THREADS, 12);
     
-    set_dbl_param(HSolver, DBL_PARAM_ABSOPTTOL, 1e-05);
-    set_dbl_param(HSolver, DBL_PARAM_ABSFEASTOL, 1e-05);
-    set_dbl_param(HSolver, DBL_PARAM_RELOPTTOL, 1e-10);
-    set_dbl_param(HSolver, DBL_PARAM_RELFEASTOL, 1e-10);
+    set_dbl_param(HSolver, DBL_PARAM_ABSOPTTOL, 1e-04);
+    set_dbl_param(HSolver, DBL_PARAM_ABSFEASTOL, 1e-04);
+    set_dbl_param(HSolver, DBL_PARAM_RELOPTTOL, 1e-06);
+    set_dbl_param(HSolver, DBL_PARAM_RELFEASTOL, 1e-06);
     set_dbl_param(HSolver, DBL_PARAM_TIMELIMIT, 3600.0);
     set_dbl_param(HSolver, DBL_PARAM_POTRHOVAL, 4.0);
     set_dbl_param(HSolver, DBL_PARAM_HSDGAMMA, 0.5);
